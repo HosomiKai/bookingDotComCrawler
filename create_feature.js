1ba558feca59b324be1d0a45d1255261cee484fb
Kai <kai@KaiYao.local>
2022-02-08 19:10:06 +0800

    feat(create feature): update create feature script

--- create_feature.js
+++ create_feature.js
@@ -1,19 +1,37 @@
-const fs = require('fs')
-
+const fs = require('fs');
 
 var lineReader = require('readline').createInterface({
   input: require('fs').createReadStream('hotelbeds.text')
 });
 
 lineReader.on('line', function (line) {
+  output = 'Feature: Hotel搜尋價格\n'
+          +'  拜訪旅館頁面\n'
+          +` Scenario: 搜尋 ${line}\n`
+          +' Given 拜訪頁面\n'
+          +' Then 看到 "您想去哪裡"\n'
+          +` Then 輸入飯店 "${line}"\n`
+          +' Then 看到 "搜尋"\n';
 
-  output = 'Feature: 拜訪頁面\n  測試用拜訪頁面\n Scenario: 測試測試\n Given 拜訪頁面\n Then 看到 "您想去哪裡"\n Then 輸入飯店 "'+ line +'"\n Then 看到 "搜尋"\n Then 查價錢\n'
-  fs.writeFile('./features/basic'+ line +'.feature', output + '   \n', { flag: 'w' }, err => {
-    if (err) {
-      console.error(err)
-      return
-    }
-  })
+  let dateString = '2022 3 3';
+  let checkInDate = new Date(dateString);
+  let now = new Date();
+  
+  if (now < checkInDate ) {
+    output += ` Then 選擇入住時間 ${dateString}\n`;
+  }
+
+  output +=' Then 查價錢\n';
+
+  let filepath = `./features/basic${line}.feature`;
+
+  fs.writeFile(filepath, output, { flag: 'w' }, 
+    err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+    });
+
   console.log('Line from file:', line);
-
 });
